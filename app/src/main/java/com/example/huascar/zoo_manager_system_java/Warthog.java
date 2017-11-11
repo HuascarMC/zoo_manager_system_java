b67ed8fdcf3e3092df24a8ca8ecf090c0d60e352
Huascar <huascarmc@hotmail.com>
2017-11-11 15:21:13 +0000

    warthog class created

---
+++ app/src/main/java/com/example/huascar/zoo_manager_system_java/Warthog.java
@@ -0,0 +1,12 @@
+package com.example.huascar.zoo_manager_system_java;
+
+/**
+ * Created by huascar on 11/11/2017.
+ */
+
+public class Warthog extends Animal {
+
+    public Warthog(int cash) {
+        super(cash);
+    }
+}
