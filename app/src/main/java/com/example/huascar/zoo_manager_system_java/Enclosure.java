2626778deef2ab7ba9732b5020c07d112d7d26d1
Huascar <huascarmc@hotmail.com>
2017-11-11 16:03:32 +0000

    added zoo class

--- app/src/main/java/com/example/huascar/zoo_manager_system_java/Enclosure.java
+++ app/src/main/java/com/example/huascar/zoo_manager_system_java/Enclosure.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 public class Enclosure<T extends Animal> {
 
-    ArrayList<T> enclosure;
+    private ArrayList<T> enclosure;
 
     public Enclosure() {
         this.enclosure = new ArrayList<>();
---
+++ app/src/main/java/com/example/huascar/zoo_manager_system_java/Zoo.java
@@ -0,0 +1,16 @@
+package com.example.huascar.zoo_manager_system_java;
+
+import java.util.ArrayList;
+
+/**
+ * Created by huascar on 11/11/2017.
+ */
+
+public class Zoo {
+
+    private ArrayList<Enclosure> enclosures;
+
+    public Zoo() {
+        this.enclosures = new ArrayList<>();
+    }
+}
