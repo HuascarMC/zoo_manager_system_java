b67fe8b55406e0ae6082a84c24db1c89aa1384ab
Huascar <huascarmc@hotmail.com>
2017-11-11 15:18:54 +0000

    jaguar class created

---
+++ app/src/main/java/com/example/huascar/zoo_manager_system_java/Jaguar.java
@@ -0,0 +1,12 @@
+package com.example.huascar.zoo_manager_system_java;
+
+/**
+ * Created by huascar on 11/11/2017.
+ */
+
+public class Jaguar extends Animal {
+
+    public Jaguar(int cash) {
+        super(cash);
+    }
+}
