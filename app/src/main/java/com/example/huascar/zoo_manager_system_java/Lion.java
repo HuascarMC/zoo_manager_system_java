148595c92a4ff9dcbac25b84e0daf8f814024e8d
Huascar <huascarmc@hotmail.com>
2017-11-11 15:19:32 +0000

    lion class createD

---
+++ app/src/main/java/com/example/huascar/zoo_manager_system_java/Lion.java
@@ -0,0 +1,12 @@
+package com.example.huascar.zoo_manager_system_java;
+
+/**
+ * Created by huascar on 11/11/2017.
+ */
+
+public class Lion extends Animal {
+
+    public Lion(int cash) {
+        super(cash);
+    }
+}
