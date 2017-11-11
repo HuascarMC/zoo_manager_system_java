a397cc238cd48e8124e7d2f913e0df7a95303f09
Huascar <huascarmc@hotmail.com>
2017-11-11 15:05:54 +0000

    animal class is abstract, cash value+

--- app/src/main/java/com/example/huascar/zoo_manager_system_java/Animal.java
+++ app/src/main/java/com/example/huascar/zoo_manager_system_java/Animal.java
@@ -4,7 +4,11 @@ package com.example.huascar.zoo_manager_system_java;
  * Created by huascar on 11/11/2017.
  */
 
-public class Animal {
+public abstract class Animal {
 
     int cash;
+
+    public Animal(int cash) {
+        this.cash = cash;
+    }
 }
